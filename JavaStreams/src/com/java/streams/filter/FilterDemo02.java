e247d9e8ddcdd4a4dee7981f52307bab0e529a09
rjimenezg73 <robertojimenezg@gmail.com>
2023-06-01 23:10:35 -0600

    stream - filter - Demo 02

---
+++ JavaStreams/src/com/java/streams/filter/FilterDemo02.java
@@ -0,0 +1,22 @@
+package com.java.streams.filter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FilterDemo02 {
+   public static void main(String[] args) {
+      List<String> names = Arrays.asList("Peter Pan", "Piolín", "Pedrito Pica Cu", "Yamada Alan", "Gaviota");
+      List<String> longNames = new ArrayList<String>();
+
+      /*longNames = names.stream().filter(str -> str.length() > 7).collect(Collectors.toList());
+      System.out.println(longNames);*/
+
+      // Otra forma
+      /*names.stream().filter(str -> str.length() > 7).forEach(s -> System.out.println(s));*/
+
+      // Algo parecido a lo anterior
+      names.stream().filter(s -> s.length() > 7).forEach(System.out::println);
+   }
+}
